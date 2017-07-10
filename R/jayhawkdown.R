ec54bd9d8978a1524dd089e412d8fb1377bc9c35
Jake Thompson <wjakethompson@gmail.com>
2017-07-10 17:15:41 -0500

    package documentation

---
+++ R/jayhawkdown.R
@@ -0,0 +1,11 @@
+#' jayhawkdown: A package for theses and dissertations for the University of Kansas using R Markdown
+#'
+#' This package creates theses/dissertations for the University of Kansas.
+#'
+#' @section thesis_gitbook:
+#' Creates an R Markdown thesis template as a webpage
+#' @section thesis_pdf:
+#' Creates an R Markdown thesis template as a PDF
+#' @docType package
+#' @name jayhawkdown
+NULL
