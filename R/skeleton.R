20f9b36b23ca66167705d5c230a872b9235ecd17
Jake Thompson <wjakethompson@gmail.com>
2020-02-15 13:38:59 -0600

    more robust copying of project files

--- R/skeleton.R
+++ R/skeleton.R
@@ -1,11 +1,14 @@
 jayhawkdown_skeleton <- function(path) {
-  # ensure directory exists
-  dir.create(path, recursive = TRUE, showWarnings = FALSE)
-
-  # copy 'resources' folder to path
+  # copy 'jayhawkdown_resources' folder to path
   resources <- jayhawkdown_file("rstudio", "templates", "project", "resources")
 
+  sub_dirs <- list.dirs(resources, recursive = TRUE,  full.names = FALSE)
+  sub_dirs <- sub_dirs[-which(sub_dirs == "")]
   files <- list.files(resources, recursive = TRUE, include.dirs = FALSE)
+
+  # ensure directories exists
+  new_path <- c(path, file.path(path, sub_dirs))
+  fs::dir_create(new_path)
 
   source <- file.path(resources, files)
   target <- file.path(path, files)
